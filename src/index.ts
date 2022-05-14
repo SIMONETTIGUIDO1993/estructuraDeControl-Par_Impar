bda4453a8e3b23cb68c94cb2262831751cee95dd
GUIDO SIMONETTI <104177337+SIMONETTIGUIDO1993@users.noreply.github.com>
2022-05-13 23:13:55 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let numero: number = Number(prompt("Ingrese un Numero"));
+
+if (numero % 2 === 0) {
+  console.log("El numero ingresado es par");
+} else {
+  console.log("El numero ingresado es impar");
+}
+if (numero <= 0) {
+  console.log("Ingrese un numero mayor a cero");
+}
